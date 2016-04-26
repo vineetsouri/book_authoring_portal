ec6a1cddb33a40d61567eaaab8a4161e38788ae5
vineetsouri <vineetsouri72@gmail.com>
2016-04-26 11:00:32 +0530

    crud operations on book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,53 @@
 class BooksController < ApplicationController
+
+  before_action :set_book, only: [:show, :edit]
+
   def index
-    
+    @all_books = Book.all
   end
+  def new
+    @book = Book.new
+    # render books_path
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+
+
+private
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit(:title, :caption)
+    end
 end
