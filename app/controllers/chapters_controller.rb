94635066e4dcc6d17d805b533eb9e4befc675c71
vineetsouri <vineetsouri72@gmail.com>
2016-04-28 12:54:41 +0530

    chapters CRUD operations also implemented dependent destroy

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,5 +1,6 @@
 class ChaptersController < ApplicationController
   before_action :fetch_book
+  before_action :fetch_chapter, only: [:show, :edit, :update, :destroy]
 
   def new
     @chapter = @book.chapters.new
@@ -15,10 +16,29 @@ class ChaptersController < ApplicationController
   end
 
   def show
-    @chapter = @book.chapters.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @chapter.update_attributes(chapter_params)
+      redirect_to book_chapter_path(@book,@chapter)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @chapter.destroy
+    redirect_to book_path(@book), notice: "Chapter was successfully deleted"
   end
 
 private
+
+  def fetch_chapter
+    @chapter = @book.chapters.find(params[:id])
+  end
 
   def fetch_book
     @book = Book.find(params[:book_id])
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ActiveRecord::Base
-  has_many :chapters
+  has_many :chapters, dependent: :destroy
   validates :title, presence: true
 end
